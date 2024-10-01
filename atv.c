691853cfcbb260f67ba6ea0320388e104df8580f
joaoConrad <joaovitorconrad@gmail.com>
2024-10-01 00:14:47 +0000

    Alteracao Script C

--- atv.c
+++ atv.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
-	printf("Hello World\n");
+	int n;
+	printf("Digite um Numero Inteiro: \n");
+	scanf("%i", &n);
+	
+	if (n % 2 == 0){
+		printf("O numero e PAR");
+	}
+	else{
+		printf("O numero e IMPAR");
+	}
+
 	return 0;
 }
